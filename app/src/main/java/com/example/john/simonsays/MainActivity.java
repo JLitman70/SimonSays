809bd87b8e9c0514ab24db3bfacbd8178075a682
JLitman70 <johnlitman307@gmail.com>
2018-03-09 20:07:58 -0600

    Added Test logic for automated touches

--- app/src/main/java/com/example/john/simonsays/MainActivity.java
+++ app/src/main/java/com/example/john/simonsays/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.john.simonsays;
 
 import android.media.SoundPool;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +22,11 @@ public class MainActivity extends AppCompatActivity {
         final SoundPool.Builder soundbuilder = new SoundPool.Builder();
         final SoundPool sounds = soundbuilder.build();
         final int beepId = sounds.load(this, R.raw.beep,1);
-        //final ImageView blue = (ImageView) findViewById(R.id.imageview_blue);
-        //final ImageView green = (ImageView) findViewById(R.id.imageview_green);
-        //final ImageView red = (ImageView) findViewById(R.id.imageview_red);
-        //final ImageView yellow = (ImageView) findViewById(R.id.imageview_yellow);
-        /*
+        final ImageView blue = (ImageView) findViewById(R.id.imageview_blue);
+        final ImageView green = (ImageView) findViewById(R.id.imageview_green);
+        final ImageView red = (ImageView) findViewById(R.id.imageview_red);
+        final ImageView yellow = (ImageView) findViewById(R.id.imageview_yellow);
+
         blue.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View arg0, MotionEvent arg1) {
@@ -29,11 +34,36 @@ public class MainActivity extends AppCompatActivity {
                     case MotionEvent.ACTION_DOWN: {
                         blue.setImageResource(R.drawable.blue_pressed);
                         sounds.play(beepId,1.0f,1.0f,1,0,1.3f);
-                        //Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
                         break;
                     }
                     case MotionEvent.ACTION_UP:{
                         blue.setImageResource(R.drawable.blue);
+                        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                        /*
+                        * Below is the code that simulates buttons being pressed, I needed a trigger,
+                         * so this was palced pn the blue button release a a proof of concept
+                        * */
+                        // Making the motion event objects
+                        long downTime = SystemClock.uptimeMillis();
+                        long eventTime = SystemClock.uptimeMillis() + 100;
+                        final MotionEvent motionEvent = MotionEvent.obtain(downTime, eventTime, MotionEvent.ACTION_DOWN, 0.0f, 0.0f, 0);
+                        final MotionEvent motionEvent2 = MotionEvent.obtain(downTime, eventTime, MotionEvent.ACTION_UP, 0.0f, 0.0f, 0);
+
+                        // dispatcing events on a delay
+                        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                red.dispatchTouchEvent(motionEvent);
+                                new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        red.dispatchTouchEvent(motionEvent2);
+                                    }
+                                }, 700);
+                            }
+                        },1000);
+                        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 
                         break;
                     }
@@ -49,8 +79,6 @@ public class MainActivity extends AppCompatActivity {
                     case MotionEvent.ACTION_DOWN: {
                         green.setImageResource(R.drawable.green_pressed);
                         sounds.play(beepId,1.0f,1.0f,1,0,1.2f);
-
-                        // Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
                         break;
                     }
                     case MotionEvent.ACTION_UP:{
@@ -71,8 +99,6 @@ public class MainActivity extends AppCompatActivity {
 
                         red.setImageResource(R.drawable.red_pressed);
                         sounds.play(beepId,1.0f,1.0f,1,0,1.1f);
-
-                        //Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
                         break;
                     }
                     case MotionEvent.ACTION_UP:{
@@ -92,10 +118,9 @@ public class MainActivity extends AppCompatActivity {
                     case MotionEvent.ACTION_DOWN: {
                         yellow.setImageResource(R.drawable.yellow_pressed);
                         sounds.play(beepId,1.0f,1.0f,1,0,1.0f);
-
-                        //Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
-                        break;
+                         break;
                     }
+
                     case MotionEvent.ACTION_UP:{
                         yellow.setImageResource(R.drawable.yellow);
 
@@ -106,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        */
+
 
     }
 }
